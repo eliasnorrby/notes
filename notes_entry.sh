453e235ab57b8e901fb744533d3158e2fc8bf374
Elias Norrby <elias.norrby@gmail.com>
2021-04-01 23:43:01 +0200

    feat: copy snippet with ctrl-y

--- notes_entry.sh
+++ notes_entry.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 file=$(./notes_tool.sh list | fzf \
+  --reverse \
+  --header '[alt-]enter: open | ctrl-y: copy snippet' \
   --preview './notes_tool.sh preview {}' \
   --preview-window 'right:60%:wrap' \
   --bind 'alt-up:preview-up,alt-down:preview-down' \
-  --bind 'ctrl-f:execute(./notes_tool.sh copy {})' \
+  --bind 'ctrl-y:execute(./notes_tool.sh copy {})' \
   --bind 'alt-enter:print-query' \
   --print-query | tail -1
 )
