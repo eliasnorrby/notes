d33a7ced9806c479b18d40a0af5172b976aba249
Elias Norrby <elias.norrby@gmail.com>
2021-04-04 14:57:40 +0200

    feat: add commit_notes.sh
    Helper to commit added or updated notes with a consistent message
    template.

---
+++ commit_notes.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+EXT=md
+
+commit() {
+  local note=$1
+  local op=$2
+  short_name="${note##notes/}"
+  short_name="${short_name%%.${EXT}}"
+  short_name=${short_name/-/ }
+  subject=${short_name%% *}
+  message=${short_name#* }
+  git add "$note"
+  git commit -m "note($subject): $op $message"
+}
+
+new_notes=$(git status --porcelain | grep '^?? notes/' | cut -d ' ' -f 2)
+modified_notes=$(git status --porcelain | grep '^ M notes/' | cut -d ' ' -f 3)
+
+for note in $new_notes; do
+  commit "$note" "add"
+done
+
+for note in $modified_notes; do
+  commit "$note" "update"
+done
