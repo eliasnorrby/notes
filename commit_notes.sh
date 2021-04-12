62092ee9e0f40512e4ce1c1bed902a25071705bc
Elias Norrby <elias.norrby@gmail.com>
2021-04-12 14:56:21 +0200

    chore: expect subdir structure in commit_notes.sh
    Replace the first '/' with a space in stead of the first '-'. Also, list
    all untracked files, to support the creation of new subdirectories.

--- commit_notes.sh
+++ commit_notes.sh
@@ -7,14 +7,14 @@ commit() {
   local op=$2
   short_name="${note##notes/}"
   short_name="${short_name%%.${EXT}}"
-  short_name=${short_name/-/ }
+  short_name=${short_name/\// }
   subject=${short_name%% *}
   message=${short_name#* }
   git add "$note"
   git commit -m "note($subject): $op $message"
 }
 
-new_notes=$(git status --porcelain | grep '^?? notes/' | cut -d ' ' -f 2)
+new_notes=$(git status --porcelain --untracked-files | grep '^?? notes/' | cut -d ' ' -f 2)
 modified_notes=$(git status --porcelain | grep '^ M notes/' | cut -d ' ' -f 3)
 
 for note in $new_notes; do
