5fafaee286f60cc17c2780d84eb9697ce4c34444
Elias Norrby <elias.norrby@gmail.com>
2021-04-12 11:45:23 +0200

    feat(gitbook): add build_summary.sh
    Builds a summary for gitbook.io

---
+++ tools/build_summary.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Build a SUMMARY.md from existing notes
+
+NOTES_DIR=notes
+
+cd "$NOTES_DIR" || exit
+
+subject_sections() {
+  local files subjects
+  subjects=$(find . -type d | remove_leading_dot | grep -v '^\.')
+
+  for subject in $subjects; do
+    echo "## $subject"
+    echo
+    note_links "$subject"
+    echo
+  done
+}
+
+remove_leading_dot() {
+  sed 's#\./##'
+}
+
+note_links() {
+  local files title
+  files=$(find "$1" -type f -name '*.md' | remove_leading_dot)
+
+  for file in $files; do
+    title=$(get_title "$file")
+    echo "* [$title]($file)"
+  done
+}
+
+get_title() {
+  grep "^# " -m1 < "$1" | sed 's/^# //'
+}
+
+read -r -d "" SUMMARY <<EOF
+# Summary
+
+Here is the summary.
+
+* [About](../README.md)
+
+---
+
+$(subject_sections)
+EOF
+
+echo "$SUMMARY"
