e149d70f5268c92880942b448f545598636384c4
Elias Norrby <elias.norrby@gmail.com>
2021-05-03 13:17:58 +0200

    feat: add commit-notes binding

--- commit_notes.sh
+++ commit_notes.sh
@@ -3,8 +3,7 @@
 EXT=md
 
 commit() {
-  local note=$1
-  local op=$2
+  local note=$1 op=$2
   short_name="${note##notes/}"
   short_name="${short_name%%.${EXT}}"
   short_name=${short_name/\// }
@@ -15,7 +14,14 @@ commit() {
 }
 
 new_notes=$(git status --porcelain --untracked-files | grep '^?? notes/' | cut -d ' ' -f 2)
-modified_notes=$(git status --porcelain | grep '^ M notes/' | cut -d ' ' -f 3)
+modified_notes=$(git status --porcelain | grep '^ M notes/' | cut -d ' ' -f 3 | grep -v 'SUMMARY.md')
+
+echo "Committing notes"
+echo
+
+if [ -z "$new_notes" ] && [ -z "$modified_notes" ]; then
+  echo "Nothing to do"
+fi
 
 for note in $new_notes; do
   commit "$note" "add"
@@ -24,3 +30,8 @@ done
 for note in $modified_notes; do
   commit "$note" "update"
 done
+
+if [ "$1" = "prompt" ]; then
+  echo
+  read -r -p "Press any key to close"
+fi
--- notes_entry.sh
+++ notes_entry.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+header_first_row='[alt-]enter: open | ctrl-y: copy snippet | ctrl-r: reload'
+header_second_row='ctrl-n: cycle snippets | ctrl-k: commit notes'
+
 file=$(./notes_tool.sh list-with-tags | fzf \
   --ansi \
   --reverse \
-  --header '[alt-]enter: open | ctrl-y: copy snippet' \
+  --header "$header_first_row"$'\n'"$header_second_row" \
   --preview './notes_tool.sh preview {1}' \
   --preview-window 'right:80:wrap' \
   --bind 'alt-up:preview-up,alt-down:preview-down' \
   --bind 'ctrl-y:execute-silent(./notes_tool.sh copy {1})+abort' \
   --bind 'ctrl-r:execute-silent(./notes_tool.sh invalidate-cache)+reload(./notes_tool.sh list-with-tags)' \
   --bind 'ctrl-n:execute-silent(./notes_tool.sh next-snippet {1})+refresh-preview' \
+  --bind 'ctrl-k:execute(./commit_notes.sh prompt >&2)+abort' \
   --bind 'alt-enter:print-query' \
   --print-query | tail -1 | cut -d " " -f 1
 )
