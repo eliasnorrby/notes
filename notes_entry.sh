dc459de2b2d900d27a12f9141745ab311c9b2c7b
Elias Norrby <elias.norrby@gmail.com>
2021-06-11 01:09:48 +0200

    feat: use ctrl-u instead of ctrl-w to commit
    I don't want to shadow the delete-backward-word of ctrl-w.

--- notes_entry.sh
+++ notes_entry.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 header_first_row='[alt-]enter: open | ctrl-y: copy snippet | ctrl-r: reload'
-header_second_row='ctrl-n: cycle snippets | ctrl-w: commit notes | ctrl-o: open in browser'
+header_second_row='ctrl-n: cycle snippets | ctrl-u: commit notes | ctrl-o: open in browser'
 
 file=$(./notes_tool.sh list-with-tags | fzf \
   --ansi \
@@ -15,7 +15,7 @@ file=$(./notes_tool.sh list-with-tags | fzf \
   --bind 'ctrl-n:execute-silent(./notes_tool.sh next-snippet {1})+refresh-preview' \
   --bind 'ctrl-o:execute-silent(./notes_tool.sh open-in-browser {1})+abort' \
   --bind 'ctrl-p:execute(git push)' \
-  --bind 'ctrl-w:execute(./commit_notes.sh prompt >&2)' \
+  --bind 'ctrl-u:execute(./commit_notes.sh prompt >&2)' \
   --bind 'ctrl-f:execute(git pull --no-rebase)' \
   --bind 'alt-enter:print-query' \
   --print-query | tail -1 | cut -d " " -f 1
